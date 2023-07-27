2e8ae5e992211b773f2a13b1f1c3b2731d624c5b
Harikrishnan Venugopal Nair <115133374+hari3100@users.noreply.github.com>
2023-07-27 15:13:12 +0530

    Delete customer.sql

--- customer.sql
+++
@@ -1,9 +0,0 @@
-create table customer(
-cust_id int primary key	auto_increment,
-Name varchar(55) not null,
-Gender varchar(10) not null,
-Age int not null,
-Address varchar(15),
-check(Gender in ('Male', 'Female', 'Unknown'))
-);
-
